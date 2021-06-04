22a832a81bcbde0aef6e5e592e041596bdd3ed5a
Andrew Morozko <Andrew-Morozko@users.noreply.github.com>
2021-06-04 05:05:00 +0300

    fixed exit signal handling issue in entrypoints

--- worker/app/entrypoint.sh
+++ worker/app/entrypoint.sh
@@ -2,11 +2,4 @@
 chown -R www:www /app/user_data/
 chown www:www /app/user_data
 
-
-# -O fair (better for long tasks, ?)
-# --prefetch-multiplier - typically long tasks, fetch 1 per worker
-#   -Q, --queues COMMA SEPARATED LIST
-#   -X, --exclude-queues COMMA SEPARATED LIST
-
-su-exec www:www python3 -m app
-# celery -A tasks worker -l INFO -Q celery --prefetch-multiplier=1 --autoscale=16,1
+exec su-exec www:www python3 -m app

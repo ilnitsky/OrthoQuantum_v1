748f0dd12471e4175b2c71a4b2663005f99faa69
Andrew Morozko <Andrew-Morozko@users.noreply.github.com>
2022-03-18 19:03:22 +0300

    fix for old kernel (setcap bind failed)

--- worker/app/entrypoint.sh
+++ worker/app/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 chown -R www:www /app/user_data/
 chown www:www /app/user_data
-cat /app/hosts_append >>/etc/hosts
-setcap 'cap_net_bind_service=+ep' /bin/proxy
 
-runuser -u www -g www --whitelist-environment "SOCKS_SERVER,SOCKS_PORT" -- proxy &
+# Proxy stuff
+cat /app/hosts_append >>/etc/hosts
+
+setcap 'cap_net_bind_service=+ep' /bin/proxy
+if [ $? -eq 0 ]; then
+    runuser -u www -g www --whitelist-environment "SOCKS_SERVER,SOCKS_PORT" -- proxy &
+else
+    # failed with setcap, have to run as root
+    proxy &
+fi
 runuser -u www -g www --whitelist-environment "REDIS_HOST,PYTHONUNBUFFERED" -- python3 -m app

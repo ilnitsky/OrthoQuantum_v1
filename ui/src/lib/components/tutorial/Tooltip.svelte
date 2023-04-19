5f29e5c4a137eadf314baeffb81fb76397d15e65
Andrew Morozko <github-msgs-web@bitnotes.xyz>
2023-04-19 04:03:37 +0400

    Tutorial toggle

--- ui/src/lib/components/tutorial/Tooltip.svelte
+++ ui/src/lib/components/tutorial/Tooltip.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import { Tooltip } from 'sveltestrap';
 	import type { TooltipPlacement } from 'sveltestrap/src/Tooltip';
+	import { showTutorial } from './TutorialToggleBtn.svelte';
 	export let target: string | HTMLElement;
 	export let placement: TooltipPlacement;
 	export let isOpen = false;
-	import { showTutorial } from './store';
 </script>
 
 {#if $showTutorial}
